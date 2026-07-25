2cbb946716f78ad78ffa73d6c04fe1bf843fa3b8
d0ngdii <aforlanes022@gmail.com>
2026-07-25 17:16:09 +0800

    Fix Loading Animation

--- js/loading.js
+++ js/loading.js
@@ -1,13 +1,22 @@
 const overlay = document.getElementById("loadingOverlay");
 
-function showLoading(title, message) {
-  loadingTitle.textContent = title;
+const loadingTitle = document.getElementById("loadingTitle");
+const loadingMessage = document.getElementById("loadingMessage");
 
+function showLoading(title, message) {
+  if (!overlay) {
+    console.error("loadingOverlay not found.");
+    return;
+  }
+
+  loadingTitle.textContent = title;
   loadingMessage.textContent = message;
 
   overlay.classList.remove("hidden");
 }
 
 function hideLoading() {
+  if (!overlay) return;
+
   overlay.classList.add("hidden");
 }
