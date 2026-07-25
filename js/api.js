ca02009d5cda8af44edd5a282cada635c81c3206
d0ngdii <aforlanes022@gmail.com>
2026-07-25 15:36:10 +0800

    Update MASATRACK frontend

--- js/api.js
+++ js/api.js
@@ -1,15 +1,31 @@
-const API_URL =
-  "https://script.google.com/macros/s/AKfycbxAko5142kaEqjaea44XASbPlsSqxWtQwqlXoiErXUcQNJZAnai6KRC_1gZM0iiXBwq/exec";
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      success: false,
+      message: "Method not allowed",
+    });
+  }
 
-async function checkAvailability(date, hikers) {
-  const response = await fetch(
-    API_URL +
-      "?action=availability" +
-      "&date=" +
-      encodeURIComponent(date) +
-      "&hikers=" +
-      encodeURIComponent(hikers),
-  );
+  try {
+    const appsScriptUrl =
+      "https://script.google.com/macros/s/AKfycbwwUcJEFCf4a6nucGB3dgUlQsPmIKP1d2zdDllckBsPGSFPmbI7SoS1o5I_9kFxI2CV/exec";
 
-  return await response.json();
-}
+    const upstream = await fetch(appsScriptUrl, {
+      method: "POST",
+      body: new URLSearchParams({
+        action: "submit",
+        payload: JSON.stringify(req.body),
+      }),
+    });
+
+    const text = await upstream.text();
+
+    res.setHeader("Content-Type", "application/json");
+    return res.status(200).send(text);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.toString(),
+    });
+  }
+};
