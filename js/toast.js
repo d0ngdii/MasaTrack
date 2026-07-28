d54e91d16bb4bb6a6a47f126088e10e6f65b75e4
d0ngdii <aforlanes022@gmail.com>
2026-07-28 17:51:37 +0800

    Debug toast notif

--- js/toast.js
+++ js/toast.js
@@ -1,6 +1,16 @@
-const toastContainer = document.getElementById("toastContainer");
+/*************************************************
+ * MASATRACK
+ * Toast Notification
+ *************************************************/
 
 function showToast(message, type = "info", duration = 3000) {
+  const toastContainer = document.getElementById("toastContainer");
+
+  if (!toastContainer) {
+    console.error("toastContainer not found.");
+    return;
+  }
+
   const toast = document.createElement("div");
 
   toast.className = `toast ${type}`;
